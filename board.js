4d8d6dd943f3c41eb64bc0828689488599b3e083
梓酱Azusa <64982342+ZiAzusa@users.noreply.github.com>
2022-10-16 20:17:18 +0800

    Update board.js

--- board.js
+++ board.js
@@ -24,7 +24,6 @@ function alertNotice(data, last_notice) {
         var exp = new Date();
         exp.setTime(exp.getTime() + 30 * 24 * 60 * 60 * 1000);
         document.cookie = "last_notice=" + last_notice  + ";expires=" + exp.toGMTString();
-        //alert("我们有新的公告！\n公告时间：" + data[last_notice].time + "\n公告内容：" + data[last_notice].content + "\n如需查看其他公告请展开首页的公告栏~");
         Swal.fire({
             title: "新公告！",
             html: "<span style=\"color: red; font-weight: bold;\">本站QQ群(当然是首位)：<a href=\"https://jq.qq.com/?_wv=1027&amp;k=G9DJBTKA\">659950593</a>  欢迎加入！！！</span><br>"+"公告时间：" + data[last_notice].time + "<br>公告内容：<span style='color: dodgerblue;font-weight: bold;'>" + noticeMessage + "</span><br>如需查看其他公告请展开首页的公告栏~",
@@ -46,7 +45,6 @@ window.onload = function () {
         }
     }
     window.loaded_notice = 0;
-    window.list_height = 0;
 }
 function changeBoard() {
     var notices = window.notices;
@@ -55,14 +53,13 @@ function changeBoard() {
         for (var i in notices) {
             var divObj = document.createElement("div");
             divObj.id = "notice_" + String(i);
-            divObj.style = "background-color:rgba(255,255,255,0.6);text-align:left;height:" + notices[i].height + "px";
+            divObj.style = "background-color:rgba(255,255,255,0.6);text-align:left;height:auto";
             divObj.innerHTML = "<p style='font-size:10px'>时间：" + notices[i].time + "</p><p>内容：" + notices[i].content + "</p>";
             list.prepend(divObj);
-            window.list_height += notices[i].height;
         }
         window.loaded_notice = 1;
     }
-    list.style.height = list.offsetHeight==window.list_height?0+"px":window.list_height+"px";
+    list.style.height = list.style.height=="auto"?"0px":"auto";
     if (list.style.height == "0px") {
         document.getElementById("board_ontrol").innerHTML = "展开公告栏";
     } else {
